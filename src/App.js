d202c78c8a488bc8a3b138c762b7d20a1f6e4479
Dubey Ravi Vinod <dubeyravivinod@gmail.com>
2023-03-26 15:02:21 +0530

    Weekly Test - 03

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import './Components/styles.css'
+import FetchAPI from './Components/FetchAPI';
+import { useState } from 'react';
 
 function App() {
+  const [opt, setOpt] = useState('all');
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className='UserDetails'>User Details</h1>
+      <p className='UserDetails'>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
+      </p>
+      <div className='Options'>
+        <input type="radio" id="html" name="fav_language" value="All" onClick={() => {setOpt('all')}} />
+        <label for="All" >All</label>
+        <input type="radio" id="css" name="fav_language" value="Male" onClick={() => {setOpt('male')}} />
+        <label for="Male" >Male</label>
+        <input type="radio" id="css" name="fav_language" value="Female" onClick={() => {setOpt('female')}}/>
+        <label for="Female" >Female</label>
+      </div>
+      <FetchAPI opt={opt}/>
     </div>
   );
 }
---
+++ src/Components/FetchAPI.js
@@ -0,0 +1,102 @@
+import { useState, useEffect } from 'react';
+import './styles.css';
+
+
+const FetchAPI = (props) => {
+    const [data, setData] = useState("");
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState("");
+    
+
+    console.log("Inside the Set API...");
+    console.log("Props Data --> ", props.opt);
+    
+    useEffect(() => {
+        
+        const url = "https://randomuser.me/api/?results=10";
+        const fetchInfo = async () => {
+            try{
+                console.log("URL :: ", url);
+                const response = await fetch ("https://randomuser.me/api/?results=10");
+                const data = await response.json();
+                console.log("Response Data:: ", data.results);
+                setData(data.results);
+                setLoading(false);
+            }
+            catch (error) {
+                console.log("Error While Fetching the Data:: ", error);
+                setError(error);
+            }
+        }
+        fetchInfo();
+
+    }, [])
+
+    if(error) {
+        return <>
+            <p style={{color: "red"}}>{error}</p>
+        </>
+    }
+    else if(loading) {
+        return <>
+            <p>Loading....</p>
+        </>
+    }
+    else if(!loading) {
+        if(props.opt !== "all"){
+        return (
+            <div className='FetchMain'>
+                <div className='mainDiv'>
+                <table>
+                    <thead>
+                    <tr>
+                        <th>IMAGE</th>
+                        <th>NAME</th>
+                        <th>EMAIL</th>
+                        <th>GENDER</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                        {data.filter(gender => gender.gender === props.opt).map(user => (
+                            <tr>
+                                <td><img src={user.picture.large} alt={user.name.first} /></td>
+                                <td>{user.name.first}</td>
+                                <td>{user.email}</td>
+                                <td>{user.gender}</td>
+                            </tr>
+                        ))} 
+                    </tbody>
+                </table>
+                </div>
+            </div>
+        );}
+        else {
+            return (<div className='FetchMain'>
+                <div className='mainDiv'>
+                <table>
+                    <thead>
+                    <tr>
+                        <th>IMAGE</th>
+                        <th>NAME</th>
+                        <th>EMAIL</th>
+                        <th>GENDER</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                        {data.map(user => (
+                            <tr>
+                                <td><img src={user.picture.large} alt={user.name.first} /></td>
+                                <td>{user.name.first}</td>
+                                <td>{user.email}</td>
+                                <td>{user.gender}</td>
+                            </tr>
+                        ))} 
+                    </tbody>
+                </table>
+                </div>
+            </div>);
+        }
+    }
+}
+
+export default FetchAPI;
